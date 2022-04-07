fddb58367d5bfd9e3261947705b13054f5980fed
rewinagebreegziabher <rewinagebreegziabher@gmail.com>
2022-04-07 04:27:38 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *
+ *  *main - entry point
+ *
+ *   *
+ *
+ *    *Return: 0 okay
+ *
+ *    
+ *
+ *  */
+
+int main(void) 
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
