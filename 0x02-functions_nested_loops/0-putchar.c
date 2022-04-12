96e215cf55851af9cfeaf60beedbb2cbb147f868
rewinagebreegziabher <rewinagebreegziabher@gmail.com>
2022-04-12 11:34:32 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *main-main block
+ *description:print -putchar in a new line
+ *return:this program returns zero
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
